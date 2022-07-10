2f72544f7aee71627838dbd82d508972368ab063
Pedrok99 <pedrohlima20@gmail.com>
2022-07-10 18:44:50 -0300

    add Chakra UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,14 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ChakraProvider>
       <Component {...pageProps} />
       <ToastContainer />
-    </>
+    </ChakraProvider>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
+import { Button, Center } from '@chakra-ui/react';
 import type { NextPage } from 'next';
-import { useEffect } from 'react';
 import { toast } from 'react-toastify';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  useEffect(() => {
-    toast.info('Projeto agenda');
-  }, []);
-
-  return <h1 className={styles.crimson}>Projeto agenda</h1>;
+  return (
+    <Center height='100vh'>
+      <Button
+        colorScheme='telegram'
+        size='lg'
+        onClick={() => toast.info('Projeto agenda')}>
+        Projeto agenda
+      </Button>
+    </Center>
+  );
 };
 
 export default Home;
