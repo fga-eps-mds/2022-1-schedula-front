0dbeb46f7f0ab08aa919f39a020934f0f10d3b6d
Pedrok99 <pedrohlima20@gmail.com>
2022-07-10 19:25:59 -0300

    add react hook form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,44 @@
-import { Button, Center } from '@chakra-ui/react';
+import {
+  Button,
+  Center,
+  Input,
+  Spacer,
+} from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import { toast } from 'react-toastify';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+type formFields = {
+  field1: string;
+};
 
 const Home: NextPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: {},
+  } = useForm<formFields>();
+  const onSubmit: SubmitHandler<formFields> = (data) => {
+    alert(JSON.stringify(data));
+    toast.info('Form enviado');
+  };
   return (
     <Center height='100vh'>
-      <Button
-        colorScheme='telegram'
-        size='lg'
-        onClick={() => toast.info('Projeto agenda')}>
-        Projeto agenda
-      </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          size='sm'
+          w='sm'
+          mr={2}
+          placeholder='Field 1'
+          {...register('field1')}
+        />
+        <Button
+          colorScheme='telegram'
+          size='sm'
+          type='submit'>
+          Projeto agenda
+        </Button>
+      </form>
     </Center>
   );
 };
