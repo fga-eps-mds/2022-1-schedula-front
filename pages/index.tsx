32b3f95d4aa51824d3e50f16f3cff990d8265c15
Pedrok99 <pedrohlima20@gmail.com>
2022-07-10 18:03:09 -0300

    linting files

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,7 @@ import type { NextPage } from 'next';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  return (
-    <h1
-      className={styles.crimson}
-      color={styles.crimson}
-      onChange={() => {
-        return;
-      }}>
-      Projeto agenda
-    </h1>
-  );
+  return <h1 className={styles.crimson}>Projeto agenda</h1>;
 };
 
 export default Home;
