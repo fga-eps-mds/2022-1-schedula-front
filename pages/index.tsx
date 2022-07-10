b87ee0e02193f48dc33a63298c60e4b4244393a4
Pedrok99 <pedrohlima20@gmail.com>
2022-07-10 18:22:28 -0300

    add toastify

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from 'next';
+import { useEffect } from 'react';
+import { toast } from 'react-toastify';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    toast.info('Projeto agenda');
+  }, []);
+
   return <h1 className={styles.crimson}>Projeto agenda</h1>;
 };
 
