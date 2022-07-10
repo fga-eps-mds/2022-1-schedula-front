7aa64668ba8062984b04627b929d398c2810fe92
Pedrok99 <pedrohlima20@gmail.com>
2022-07-10 17:41:07 -0300

    initial lint config

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,18 @@ import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const test = 'test'
+  console.log(test)
+
   return (
-   <h1 className={styles.crimson}>Projeto agenda</h1>
+    <h1
+      className={styles.crimson}
+      color={styles.crimson}
+      onChange={() => {
+        return
+      }}>
+      Projeto agenda
+    </h1>
   )
 }
 
