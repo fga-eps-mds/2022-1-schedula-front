cd321f7601101beff70d5afa6051b55ee8eee169
RodrigoWright <wright.rodrigo380@gmail.com>
2022-07-20 17:44:39 -0300

    Titulo Schedula

--- src/pages/lista_categoria.tsx
+++ src/pages/lista_categoria.tsx
@@ -1,9 +1,13 @@
+import { Heading } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 
 const lista_categoria: NextPage = () => {
 
     return(
-        <div>
+        <div style = {{margin: '55px'}}>
+            <div style = {{boxSizing: 'border-box', borderBottom: '1px solid', width: '360px', height: '60px'}}>
+                <Heading width={210} height={51} margin = '0 auto' size='xl' textAlign='center'>Schedula</Heading>
+            </div>
         </div>
     );
 }
