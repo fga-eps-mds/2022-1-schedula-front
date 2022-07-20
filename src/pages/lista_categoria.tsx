4d7f1576fc079fd625dd5691f2ed6be1761468a6
RodrigoWright <wright.rodrigo380@gmail.com>
2022-07-20 18:34:35 -0300

    Tiulo Schedula

--- src/pages/lista_categoria.tsx
+++ src/pages/lista_categoria.tsx
@@ -1,14 +1,31 @@
-import { Heading } from '@chakra-ui/react';
+import { Heading, Box, Flex, Button, color } from '@chakra-ui/react';
 import type { NextPage } from 'next';
+
 
 const lista_categoria: NextPage = () => {
 
+    const MenuHeadStyle ={
+        boxSizing: 'border-box', 
+        borderBottom: '1px solid', 
+        width: '360px', 
+        height: '60px',
+        borderColor: '#777777'
+    }
+
     return(
-        <div style = {{margin: '55px'}}>
-            <div style = {{boxSizing: 'border-box', borderBottom: '1px solid', width: '360px', height: '60px'}}>
-                <Heading width={210} height={51} margin = '0 auto' size='xl' textAlign='center'>Schedula</Heading>
-            </div>
+        <div Background-color='#E5E5E5' style = {{margin: '55px'}}>
+            <Flex 
+                align="center" 
+                justify="center" 
+                style = {MenuHeadStyle}>
+                <Heading width={210} height={51} margin = '0 auto' size='xl' textAlign='center'>
+                    Schedula
+                </Heading>
+            </Flex>
+
         </div>
+
+        
     );
 }
 
