ade8ec08726150ae70bc5d4103034eef339e8782
RodrigoWright <wright.rodrigo380@gmail.com>
2022-07-20 21:36:43 -0300

    Layout schedula

---
+++ src/layout/DefaultLayout.tsx
@@ -0,0 +1,65 @@
+import { Heading, Flex, Button, Stack } from '@chakra-ui/react';
+import type { NextPage } from 'next';
+import { DashbordOptions } from '../styles/components/DashbordOptions';
+import { ReactNode } from 'react';
+
+interface DefaultLayoutProps {
+    children: ReactNode
+}
+
+const DefaultLayout = ({children}: DefaultLayoutProps) => {
+
+    const MenuHeadStyle ={
+        boxSizing: 'border-box', 
+        borderBottom: '1px solid', 
+        width: '360px', 
+        height: '60px',
+        borderColor: '#777777'
+    }
+
+    return(
+        <>
+        <div style={{display: 'flex'}}>
+            <div Background-color='#E5E5E5' style = {{margin: '55px'}}>
+                <Flex 
+                    align="center" 
+                    justify="center" 
+                    style = {MenuHeadStyle}>
+                    <Heading width={210} height={51} margin = '0 auto' size='xl' textAlign='center'>
+                        Schedula
+                    </Heading>
+                </Flex>
+
+                <Stack spacing={4} direction='column' align='left' w={360}>
+                    <DashbordOptions isActive>
+                        Dashbord
+                    </DashbordOptions>
+                    <DashbordOptions>
+                        Chamados
+                    </DashbordOptions>
+                    <DashbordOptions>
+                        Registrar Chamados
+                    </DashbordOptions>
+                    <DashbordOptions>
+                        Gerenciar Tipos De Chamados
+                    </DashbordOptions>
+                    <DashbordOptions>
+                        Tutoriais
+                    </DashbordOptions>
+                </Stack>
+
+
+            </div>
+
+            <main style = {{width: '100%'}}>{children}</main>
+        </div>
+
+        {/* <footer style = {{bottom: '100%'}}>
+            Hello World
+        </footer> */}
+
+        </>
+    );
+}
+
+export default DefaultLayout
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,27 @@ import { ChakraProvider } from '@chakra-ui/react';
 
 import '../styles/globals.css';
 import 'react-toastify/dist/ReactToastify.css';
+import { ColorTheme } from '../styles/ColorTheme';
+import { ReactElement, ReactNode } from 'react';
+import type { NextPage } from 'next';
 
-function MyApp({ Component, pageProps }: AppProps) {
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+  auth?: boolean;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({
+  Component,
+  pageProps,
+}: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider resetCSS theme={ColorTheme}>
+      {getLayout(<Component {...pageProps} />)}
       <ToastContainer />
     </ChakraProvider>
   );
