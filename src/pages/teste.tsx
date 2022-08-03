0d575457708423e39747f278788400e3b1802d33
RodrigoWright <wright.rodrigo380@gmail.com>
2022-08-03 12:21:27 -0300

    Testing modal type cad

--- src/pages/teste.tsx
+++ src/pages/teste.tsx
@@ -1,20 +1,58 @@
-import { ReactNode } from 'react';
-import { Heading } from '@chakra-ui/react';
+import { ReactNode, useState } from 'react';
+import {
+  Box,
+  Button,
+  Heading,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react';
+
+import { Data1 } from '@components/DataType';
+import { ModalCadType } from '@components/ModalCadType';
 
 import DefaultLayout from '../layout/DefaultLayout';
 
-const teste = () => {
+const Teste = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [categorias, setCategorias] = useState<Data1[]>([]);
+
+  function callBack(categoria: Data1) {
+    setCategorias([categoria, ...categorias]);
+  }
+
   return (
     <>
       <Heading w='100%'>Página Teste</Heading>
+      <Button onClick={onOpen}>Teste Modal Cad.</Button>
+      <ModalCadType
+        isOpen={isOpen}
+        onOpen={onOpen}
+        onClose={onClose}
+        callBack={callBack}
+      />
+      <Box mt='1em' mb='3em'>
+        <Text>Tipos cadastrados no sitema.</Text>
+        {
+          //o map só pode listar se tiver com o active == true, não faz isso agora pq a api não existe.
+        }
+        {categorias?.map((categoria: Data1) => {
+          // categoria.active === true ?
+          return (
+            <>
+              <Text>{categoria.name}</Text>
+              <Text>{categoria.description}</Text>
+            </>
+          );
+        })}
+      </Box>
     </>
   );
 };
 
-teste.getLayout = (page: ReactNode) => {
+Teste.getLayout = (page: ReactNode) => {
   return (
     <DefaultLayout Active='dashboard'>{page}</DefaultLayout>
   );
 };
 
-export default teste;
+export default Teste;
