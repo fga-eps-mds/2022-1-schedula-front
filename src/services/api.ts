518c963c188bb7195f4d2e551a6818d0d7cec607
EduardoPicolo <eduardonpicolo@gmail.com>
2022-08-14 20:57:48 -0300

    Create axios error handler
    Co-authored-by: Sayuck <nobrega.m.roberto@gmail.com>
    Co-authored-by: Pedrok99 <pedrohlima20@gmail.com>

---
+++ src/services/api.ts
@@ -0,0 +1,38 @@
+import { toast } from 'react-toastify';
+import axios from 'axios';
+
+const api = axios.create({
+  //   withCredentials: true,
+});
+
+const errorResponseHandler = (error: any) => {
+  if (error?.response) {
+    if (typeof error?.response?.data === 'string') {
+      return Promise.reject(new Error(error.response.data));
+    }
+
+    return Promise.reject(
+      new Error('Something went wrong', {
+        cause: error.response,
+      })
+    );
+  }
+
+  if (error?.request) {
+    console.error('INTERNAL SERVER ERROR: ', error?.toJSON?.());
+
+    toast.error('Internal Server Error', {
+      containerId: 'fetch-error',
+      toastId: 'internal-server-error',
+    });
+
+    return Promise.reject(new Error('Internal server error'));
+  }
+
+  return Promise.reject(error);
+};
+
+// api.defaults.withCredentials = true;
+api.interceptors.response.use((response) => response, errorResponseHandler);
+
+export { api };
