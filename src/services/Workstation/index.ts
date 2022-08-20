bffb215fe3d1060a3c6dcafbbbfa5467db2a7df8
Guilherme de Oliveira Mendes <guioliveira20082009@gmail.com>
2022-08-20 18:39:27 -0300

    arrumando o código como pedido no PR

--- src/services/Workstation/index.ts
+++ src/services/Workstation/index.ts
@@ -2,22 +2,22 @@
 import { createRequestConfig } from '@services/request';
 
 export const getWorkstation = createRequestConfig({
-  url: "/workstation",
-  method: "get"
+  url: '/workstation',
+  method: 'get'
 })
 
 export const createWorkstation = createRequestConfig<CreateWorkstationPayload>({
-  url: "/workstation",
-  method: "post"
+  url: '/workstation',
+  method: 'post'
 })
 
 export const updateWorkstation = (id: number) =>
   createRequestConfig<CreateWorkstationPayload>({
-    url: "/workstation/${id}",
-    method: "put"
+    url: `/workstation/${id}`,
+    method: 'put'
   })
 
 export const deleteWorkstation = (id: number) => ({
-  url: "/workstation/${id}",
-  method: "delete"
+  url: `/workstation/${id}`,
+  method: 'delete'
 })
