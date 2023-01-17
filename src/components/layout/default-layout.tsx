203d1c39737bad23c76cadfd1e9d161d5ef8d490
Paulo Victor <paulovmel@gmail.com>
2023-01-16 23:51:40 -0300

    feat: adds automatic signout for inativity functionality

--- src/components/layout/default-layout.tsx
+++ src/components/layout/default-layout.tsx
@@ -2,29 +2,32 @@ import { Flex, Grid } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
 import { SideBar } from '@/components/side-bar';
 import Footnote from '@/features/api-status/components/footnote';
+import { AutoLogoutComponent } from '@/components/auto-logout-component';
 
 export function DefaultLayout() {
   return (
-    <Flex
-      as="main"
-      justifyContent="center"
-      p={14}
-      minHeight="100vh"
-      overflow="clip"
-    >
-      <Grid
-        width="100%"
-        maxWidth="1440px"
-        templateColumns="auto minmax(0, 1fr)"
-        gap={14}
+    <AutoLogoutComponent>
+      <Flex
+        as="main"
+        justifyContent="center"
+        p={14}
+        minHeight="100vh"
+        overflow="clip"
       >
-        <SideBar />
-        <Flex flexDirection="column">
-          <Outlet />
-        </Flex>
-      </Grid>
+        <Grid
+          width="100%"
+          maxWidth="1440px"
+          templateColumns="auto minmax(0, 1fr)"
+          gap={14}
+        >
+          <SideBar />
+          <Flex flexDirection="column">
+            <Outlet />
+          </Flex>
+        </Grid>
 
-      <Footnote />
-    </Flex>
+        <Footnote />
+      </Flex>
+    </AutoLogoutComponent>
   );
 }
